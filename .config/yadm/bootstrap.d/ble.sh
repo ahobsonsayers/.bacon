fa68f8931a07e250d962d8467305819423879156
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2023-08-17 11:37:13 +0100

    Update bootstrap

--- .config/yadm/bootstrap.d/ble.sh
+++ .config/yadm/bootstrap.d/ble.sh
@@ -11,7 +11,7 @@ echo Installing ble.sh
 echo ----------------------
 
 if ! [[ -r ~/.local/share/blesh/ble.sh ]]; then
-    if ! [[ -d ~/.ble.sh ]]; then
+    if ! [[ -d ~/ble.sh ]]; then
         git clone --recursive --depth 1 --shallow-submodules https://github.com/akinomyoga/ble.sh.git
     fi
     make -C ble.sh install PREFIX=~/.local
